1fa0f42d198aaeff00b19a9b8461b37b9fd006ff
nobbs00 <nobbs@126.com>
2025-02-24 15:25:20 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -139,7 +139,7 @@ echo "TARGET_DEVICE: $TARGET_DEVICE"
 
 if [ $KSU_ENABLE -eq 1 ]; then
     echo "KSU is enabled"
-    curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/refs/heads/next/kernel/setup.sh" | bash -s
+    curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/refs/heads/main/kernel/setup.sh" | bash -s v0.9.5
 else
     echo "KSU is disabled"
 fi
